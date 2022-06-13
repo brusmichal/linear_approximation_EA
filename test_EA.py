71dde839c77dd35a767481157e5ee660ebfb039e
brusmichal <brus.michal@outlook.com>
2022-06-13 12:32:58 +0200

    saving to csv enabled in make_statistics

--- test_EA.py
+++ test_EA.py
@@ -1,5 +1,6 @@
 import math
 import numpy as np
+import pandas as pd
 from pprint import pprint
 from EvolutionAlgorithm import Evolution
 import cec2017.functions as cec
@@ -16,40 +17,45 @@ def f2(x):
 
 def main():
     # test_ea()
-    make_statistics(1)
+    make_statistics(1, with_print=False)
 
 
-def make_statistics(runs_number):
+def make_statistics(runs_number, with_print):
     populations = np.array([20, 50, 80, 100, 120])
     mutations = np.array([0.05, 0.1, 0.5, 1, 2])
     means = np.empty((len(cec.all_functions), len(populations), len(mutations)))
     stds = np.empty((len(cec.all_functions), len(populations), len(mutations)))
-    print(f"Populations: {populations}")
-    print(f"Mutation strengths: {mutations}\n")
-    for l, function in enumerate(cec.all_functions):
-        print(f"Function {function}:\n")
+    if with_print:
+        print(f"Populations: {populations}")
+        print(f"Mutation strengths: {mutations}\n")
+    for l, function in enumerate(cec.all_functions[0:1]):
+        if with_print:
+            print(f"Function {function}:\n")
         for k, population in enumerate(populations):
             # print(f"Population size: {population}")
             for j, mutation in enumerate(mutations):
                 # print(f"Mutation strength: {mutation}")
                 minimums = np.empty([runs_number])
                 for i in range(runs_number):
-                    ea = Evolution(function, 10, 100, population, 10000 / population, False, mutation, 0.8)
+                    ea = Evolution(function, 10, 100, population, 10000 / population, True, mutation, 0.8)
                     ea.learn()
                     minimum = ea.get_optimum()[1]
                     minimums[i] = minimum
                 means[l][k][j] = minimums.mean()
                 stds[l][k][j] = minimums.std()
-        print("Means:")
-        print(np.array_str(means[l], precision=2))
-        print("Standard deviations:")
-        print(np.array_str(stds[l], precision=2))
-        best_params = np.unravel_index(means[l].argmin(), means[l].shape)
-        print(f"Best parameters position: {best_params[0], best_params[1]}")
-        print(f"Best parameters values (pop_size, mut_str): {populations[best_params[0]], mutations[best_params[1]]}")
-        print(f"Mean minimum with these params: {means[l][best_params[0]][best_params[1]]}")
-        print("--------------------------------------------------------------------------------------------------")
-
+        if with_print:
+            print("Means:")
+            print(np.array_str(means[l], precision=2))
+            print("Standard deviations:")
+            print(np.array_str(stds[l], precision=2))
+            best_params = np.unravel_index(means[l].argmin(), means[l].shape)
+            print(f"Best parameters position: {best_params[0], best_params[1]}")
+            print(
+                f"Best parameters values (pop_size, mut_str): {populations[best_params[0]], mutations[best_params[1]]}")
+            print(f"Mean minimum with these params: {means[l][best_params[0]][best_params[1]]}")
+            print("--------------------------------------------------------------------------------------------------")
+        pd.DataFrame(means[l]).to_csv('stats/means/means' + str(l) + '.csv')
+        pd.DataFrame(stds[l]).to_csv('stats/stds/stds' + str(l) + '.csv')
 
 
 def test_ea():
